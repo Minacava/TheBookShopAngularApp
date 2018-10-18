5b51dcde948f0fc088e6b594b0384776e2a4fde2
Minnacava <marinacamachovarea@outlook.com>
2018-10-18 19:13:00 +0200

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: 'AIzaSyAEnrec_2PTjufJIrorQHgi9Di8fdDo-xA',
     authDomain: 'loginapp-3753e.firebaseapp.com',
