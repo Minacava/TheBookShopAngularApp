7728628d243309b2b5f1a4f02b67643ce6a20d76
Minnacava <marinacamachovarea@outlook.com>
2018-10-18 22:48:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/'));
+app.use(express.static('/dist/thebook-store'));
 
 app.get('/*all', function(req,res) {
     
