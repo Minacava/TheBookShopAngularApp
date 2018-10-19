6d7db3973dd4bab1df5222e1e53435fd9eb6466e
Minnacava <marinacamachovarea@outlook.com>
2018-10-19 17:54:45 +0200

    testing server.js 1

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 //Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*all', function(req,res) {
+app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/index.html'));
+res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
