ba186f991ee70cf5a1c6ccf1299587f12c99f8ba
Minnacava <marinacamachovarea@outlook.com>
2018-11-20 11:57:41 +0100

    change dist folder from last css changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/StudioGhibli/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
 
 // Start the app by listening on the default Heroku port
